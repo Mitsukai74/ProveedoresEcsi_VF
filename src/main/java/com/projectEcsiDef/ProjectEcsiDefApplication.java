9ce7f328bae49bf22e901952d374838c1096ee6e
Mitsukai74 <ansaanva@gmail.com>
2025-05-27 19:58:43 -0500

    Iniciando el repo

---
+++ src/main/java/com/projectEcsiDef/ProjectEcsiDefApplication.java
@@ -0,0 +1,13 @@
+package com.projectEcsiDef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcsiDefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEcsiDefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectEcsiDef/ProjectEcsiDefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectEcsiDef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEcsiDefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
