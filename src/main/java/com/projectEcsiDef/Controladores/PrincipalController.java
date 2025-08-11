90c59a7803825105a8cce85cdbf0dc546c33c063
Mitsukai74 <ansaanva@gmail.com>
2025-08-10 19:32:22 -0500

    Arreglando formulario de usuario

--- src/main/java/com/projectEcsiDef/Controladores/PrincipalController.java
+++ src/main/java/com/projectEcsiDef/Controladores/PrincipalController.java
@@ -1,7 +1,11 @@
 package com.projectEcsiDef.Controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.projectEcsiDef.Entidades.Users;
+
 
 @Controller
 public class PrincipalController {
@@ -9,5 +13,16 @@ public class PrincipalController {
 	@GetMapping(path = "/test")
 	public String mostrarPlantilla() {
 		return "inicio";
+	}
+	
+	@GetMapping("/pruebaForms")
+	public String crearUser(Model model){
+		
+		Users usuario = new Users();
+		
+		model.addAttribute("titulo","Formulario: Nuevo usuario");
+		model.addAttribute("usuario",usuario);
+				
+		return "/usrVistas/formUser";
 	}
 }
